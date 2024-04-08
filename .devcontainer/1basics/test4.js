73535d0dbf8238e65934ef3291a641fa05fbb67f
developerrPrabh <161615550+developerrPrabh@users.noreply.github.com>
2024-04-08 17:57:17 +0000

    datatypes.

---
+++ .devcontainer/1basics/test4.js
@@ -0,0 +1,28 @@
+//Data Types comparisons.
+
+//console.log(2>1);
+//console.log(2==1);
+
+//console.log("2" > 1)
+
+/*console.log(undefined > 0);
+console.log(undefined==0);
+console.log(undefined < 0);
+*/
+console.log("1"==1);
+console.log("1"===1);
+
+ let id= Symbol('123');
+
+const hero= ["a", "b", "c"];        //Array declared.
+
+let myObj= {
+              name: "prabh",
+              age:  23,              // object declared
+}
+
+myObj.name= "aayush";
+
+const myFunction = function() {
+       console.log("Hello World");
+}
